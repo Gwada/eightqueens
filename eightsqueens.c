1dd8d542e1638a931bc70a22ff5dac41594a878f
Gwada <dimitri.lc@orange.fr>
2017-10-23 10:55:29 +0200

    ajout projet des 8 dames

---
+++ eightsqueens.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int		ft_print_result(int *tab)
+{
+	int		i;
+
+	i = 0;
+	while (i < 8)
+		printf("%d ", tab[i++]);
+	printf("\n");
+	return (1);
+}
+
+int		ft_is_safe(int *tab, int Q)
+{
+	int		i;
+	int		poss;
+
+	i = -1;
+	poss = 0;
+	tab[Q] = 1;
+	if (Q == 8)
+		return (ft_print_result(tab));
+	while (tab[Q] <= 8)
+	{
+		if (++i == Q)
+		{
+			poss += ft_is_safe(tab, Q + 1);
+			tab[Q]++;
+			i = -1;
+		}
+		else if ((tab[Q] == tab[i])
+		|| (tab[Q] == tab[i] - (Q - i))		//verif diag inf
+		|| (tab[Q] == tab[i] + (Q - i)))	//verif diag sup
+		{
+			tab[Q]++;
+			i = -1;
+		}
+	}
+	return (poss);
+}
+
+int		ft_eight_queens_puzzle(void)
+{
+	int		tab[8];
+
+	return (ft_is_safe(tab, 0));
+}
+
+int		main(void)
+{
+	printf("\n%d possibilites\n", ft_eight_queens_puzzle());
+	return (0);
+}
